9926bc71365309de817358cfa8fc11c845f9ebd8
furabonos <readyordie3974@gmail.com>
2020-08-05 10:27:21 +0900

    resetting

---
+++ SubwayMessenger/Source/Service/Service.swift
@@ -0,0 +1,9 @@
+//
+//  Service.swift
+//  SubwayMessenger
+//
+//  Created by Euijae Hong on 2020/08/05.
+//  Copyright © 2020 Taehyeong. All rights reserved.
+//
+
+import Foundation
---
+++ SubwayMessenger/Source/View/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  SubwayMessenger
+//
+//  Created by Euijae Hong on 2020/08/05.
+//  Copyright © 2020 Taehyeong. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
